e54bd98aff2ee06f0d9cad277aa6c152e1f4a66b
Jhon <jhon@lepidus.com.br>
2024-11-13 14:49:35 -0400

    Sets default command timeout to new value before iterations execution
    Issue: documentacao-e-tarefas/scielo#719
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/tests/Test1_replacementOfTerm.cy.js
+++ cypress/tests/Test1_replacementOfTerm.cy.js
@@ -1,5 +1,8 @@
 describe('Replacement of term in user interface', function () {
-	it('Checks presence of terms to be replaced in public site', function () {
+	before(function() {
+        Cypress.config('defaultCommandTimeout', 4000);
+    });
+    it('Checks presence of terms to be replaced in public site', function () {
         cy.visit('');
 
         cy.contains('Latest preprints');
