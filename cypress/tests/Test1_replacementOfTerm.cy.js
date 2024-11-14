58cc65b5daacbe6287acd9a6aa5b9983ed68a69c
Jhon <jhon@lepidus.com.br>
2024-11-14 16:30:36 -0400

    Removes command which altered cypress timeout
    Issue: documentacao-e-tarefas/scielo#719
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

--- cypress/tests/Test1_replacementOfTerm.cy.js
+++ cypress/tests/Test1_replacementOfTerm.cy.js
@@ -1,7 +1,4 @@
 describe('Replacement of term in user interface', function () {
-	before(function() {
-        Cypress.config('defaultCommandTimeout', 4000);
-    });
     it('Checks presence of terms to be replaced in public site', function () {
         cy.visit('');
 
